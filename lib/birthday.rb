98cf3d2dab85d56883119eba93fa0bef4162681a
Emilia Ziemichód <emifrompoland@gmail.com>
2019-03-19 04:04:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-   if age <= 12
     birthday_kids.each do |kids_name, age|
+      if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
